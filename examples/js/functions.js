b24ff346e9b0a858950685dedc6c750a7af18b31
Amy Cotton <i7673459@bournemouth.ac.uk>
2015-06-02 18:22:59 +0100

    Sorted audio and video

--- examples/js/functions.js
+++ examples/js/functions.js
@@ -3,17 +3,57 @@ var theta = $(window).scrollTop() / 10 % Math.PI;
 $('#sun').css({ transform: 'rotate(' + theta + 'rad)' });
 });
 
-$("#nav-two a")
-  .each(function(i) {
-    if (i != 0) {
-      $("#beep-two")
-        .clone()
-        .attr("id", "beep-two" + i)
-        .appendTo($(this).parent());
-    }
-    $(this).data("beeper", i);
-  })
-  .mouseenter(function() {
-    $("#beep-two" + $(this).data("beeper"))[0].play();
-  });
-$("#beep-two").attr("id", "beep-two0");
+(function($) {
+        $.jInvertScroll(['.scroll'],        // an array containing the selector(s) for the elements you want to animate
+            {
+            height: 6000,                   // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
+            onScroll: function(percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
+                console.log(percent);
+            }
+        });
+    }(jQuery));
+        
+var radio = document.getElementById("radio"); 
+function playRadio() { 
+    if (radio.paused) 
+        radio.play(); 
+    else 
+        radio.pause(); 
+} 
+        
+var guitar = document.getElementById("guitar"); 
+function playGuitar() { 
+    if (guitar.paused) 
+        guitar.play(); 
+    else 
+        guitar.pause(); 
+} 
+
+//$('#sketch1').hover(function toggleControls() {
+//    if (this.hasAttribute("controls")) {
+//        this.removeAttribute("controls")
+//    } else {
+//        this.setAttribute("controls", "controls")
+//    }
+//})
+//$('#sketch2').hover(function toggleControls() {
+//    if (this.hasAttribute("controls")) {
+//        this.removeAttribute("controls")
+//    } else {
+//        this.setAttribute("controls", "controls")
+//    }
+//})
+//$('#sketch3').hover(function toggleControls() {
+//    if (this.hasAttribute("controls")) {
+//        this.removeAttribute("controls")
+//    } else {
+//        this.setAttribute("controls", "controls")
+//    }
+//})
+//$('#sketch4').hover(function toggleControls() {
+//    if (this.hasAttribute("controls")) {
+//        this.removeAttribute("controls")
+//    } else {
+//        this.setAttribute("controls", "controls")
+//    }
+//})
